064c16b8b5b3a659544df725e818fb6517fb8753
facufierro <fierrof_47@hotmail.com>
2024-11-10 00:01:34 +0100

    feat macros add magic initiate for druid and cleric

---
+++ src/macros/feats/origin/magic-initiate-cleric.js
@@ -0,0 +1,6 @@
+fft.getCompendiumItems("fftweaks.spells", { tagsList: ["Cleric"] })
+    .then(items => fft.chooseItemsDialog(items.filter(item => item.system.level === 0), maxSelections = 2, showIcon = true, showTooltip = true))
+    .then(chosenCantrips => fft.addItemsToActor(actor, chosenCantrips))
+    .then(() => fft.getCompendiumItems("fftweaks.spells", { tagsList: ["Cleric"] }))
+    .then(items => fft.chooseItemsDialog(items.filter(item => item.system.level === 1), maxSelections = 1, showIcon = true, showTooltip = true))
+    .then(chosenLevel1Spells => fft.addItemsToActor(actor, chosenLevel1Spells));
