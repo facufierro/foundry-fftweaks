dd1721b9cc83d583e766cf543cbbc747515a7135
facufierro <fierrof_47@hotmail.com>
2025-08-29 02:16:26 +0200

    feat: comment out unused addon and module initializations in main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,13 @@ declare const dnd5e: any;
 Hooks.once("ready", () => {
     (window as any).FFT = FFT;
     // Addons
-    FFT.ItemMacroAddon.initialize();
+    // FFT.ItemMacroAddon.initialize();
     FFT.LevelsAddon.initialize();
     // FFT.SimpleCalendarAddon.initialize();
     // FFT.SimpleTimekeepingAddon.initialize();
     // Modules
     FFT.FunctionBarModule.initialize();
-    FFT.CharacterAnvilModule.initialize();
+    // FFT.CharacterAnvilModule.initialize();
     // FFT.ResizeHandler.initialize();
     FFT.TokenVisualsModule.initialize();
     FFT.TargetPickerModule.initialize();
