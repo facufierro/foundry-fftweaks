7252f49bd553e61f92bac9a7a5e65fcf68bae0c5
facufierro <fierrof_47@hotmail.com>
2025-08-31 15:16:11 +0200

    feat: initialize MidiAutoRollModule in the ready hook

--- src/main.ts
+++ src/main.ts
@@ -17,5 +17,6 @@ Hooks.once("ready", () => {
     // FFT.ResizeHandler.initialize();
     FFT.TokenVisualsModule.initialize();
     FFT.TargetPickerModule.initialize();
+    FFT.MidiAutoRollModule.initialize();
     // FFT.BattlemapGeneratorModule.initialize();
 });
