f22e9551e8276644d142df19bea0005b8b056a03
Facundo Fierro <fierrof_47@hotmail.com>
2026-01-17 12:49:53 -0300

    feat: Add combat management functions for adding, removing, and resetting tokens

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,7 +1,15 @@
 import { healTokens } from "./chracters/health/heal-tokens";
 import { hurtTokens } from "./chracters/health/hurt-tokens";
+import { addTokensToCombat } from "./combat/add-tokens-to-combat";
+import { removeTokensFromCombat } from "./combat/remove-tokens-from-combat";
+import { resetCombatEncounter } from "./combat/reset-combat-encounter";
+import { deleteCombatEncounter } from "./combat/delete-combat-encounter";
 
 export class Functions {
     static healTokens = healTokens;
     static hurtTokens = hurtTokens;
+    static addTokensToCombat = addTokensToCombat;
+    static removeTokensFromCombat = removeTokensFromCombat;
+    static resetCombatEncounter = resetCombatEncounter;
+    static deleteCombatEncounter = deleteCombatEncounter;
 }
