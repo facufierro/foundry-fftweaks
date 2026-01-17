55aa6da0ed7b45cf9b61de4a530c1dc229c5048d
Facundo Fierro <fierrof_47@hotmail.com>
2026-01-17 14:06:52 -0300

    feat: Add TargetPicker module for enhanced target selection and visual range indicators

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { Debug } from "./utils/debug";
 // Modules
 import { Functions } from "./functions";
 import { FunctionBar } from "./modules/function-bar";
+import { TargetPicker } from "./modules/target-picker";
 
 const FFT = ((globalThis as any).FFT ??= {});
 FFT.Debug = Debug;
@@ -16,5 +17,5 @@ Hooks.once("init", () => {
 Hooks.once("ready", async () => {
     Debug.Log("FFTweaks | Initializing Ready Hooks");
     await FunctionBar.initialize();
-
+    TargetPicker.initialize();
 });
