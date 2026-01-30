f6bd45a7eb83440ca8fbc62c9ec7f0304c93ddc7
Facundo Fierro <fierrof_47@hotmail.com>
2026-01-30 09:41:23 +0000

    fix: Update Resourceful feature to check for an item instead of an effect after a long rest.

--- src/plugins/automations/features/species/human/resourceful.ts
+++ src/plugins/automations/features/species/human/resourceful.ts
@@ -1,6 +1,6 @@
 export function resourceful() {
     (Hooks as any).on("dnd5e.restCompleted", (actor: any, result: { longRest: boolean }) => {
-        if (result.longRest && actor.effects.getName("Resourceful")) {
+        if (result.longRest && actor.items.getName("Resourceful")) {
             actor.update({ "system.attributes.inspiration": true });
         }
     });
