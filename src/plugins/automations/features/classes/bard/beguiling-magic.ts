095f1241f4da9b1c18890673fe1bd8c5da4f71ac
Facundo Fierro <fierrof_47@hotmail.com>
2026-01-31 19:09:57 +0000

    remove debug from beguilingmagic

--- src/plugins/automations/features/classes/bard/beguiling-magic.ts
+++ src/plugins/automations/features/classes/bard/beguiling-magic.ts
@@ -2,12 +2,10 @@ import { Functions } from "../../../../../functions";
 
 export function beguilingMagic() {
     (Hooks as any).on("dnd5e.useItem", (item: any) => {
-        console.log("FFTweaks | Hook dnd5e.useItem fired", item);
         Functions.beguilingMagic(item);
     });
 
     (Hooks as any).on("dnd5e.postUseActivity", (activity: any, usageConfig: any, results: any) => {
-        console.log("FFTweaks | Hook dnd5e.postUseActivity fired", activity);
         // In dnd5e v3, activity.item is the item
         if (activity?.item) {
             Functions.beguilingMagic(activity.item);
--- src/plugins/automations/index.ts
+++ src/plugins/automations/index.ts
@@ -3,7 +3,6 @@ import { beguilingMagic } from './features/classes/bard/beguiling-magic';
 
 export class Automations {
     static initialize() {
-        console.log("FFTweaks | Automations Initializing...");
         resourceful();
         beguilingMagic();
     }
